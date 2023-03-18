7a8eb81dd0692191f75e38bd4db9f4e7e4b5fd64
kumarigeetanjli <84951879+kumarigeetanjli@users.noreply.github.com>
2023-03-18 23:43:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const isPowerOfTwo = (number) => {
-  //your code here
-};
-
-const num = parseInt(prompt("Enter a number"));
-alert(isPowerOfTwo(num));
+function isPowerOfTwo(number) {
+  if (number <= 0) {
+    return false;
+  }
+  return (number & (number - 1)) === 0;
+}
